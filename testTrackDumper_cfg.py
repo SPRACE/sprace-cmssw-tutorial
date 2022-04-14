b5da460bb606b432c04834e840d0cd24f5bef266
Thiago Tomei <Thiago.Tomei@cern.ch>
2022-04-14 18:02:21 -0300

    Add testTrackDumper_cfg.py

---
+++ testTrackDumper_cfg.py
@@ -0,0 +1,25 @@
+import FWCore.ParameterSet.Config as cms
+
+# process
+process = cms.Process("TEST")
+
+# number of events to be processed and source file
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(400)
+)
+
+process.source = cms.Source("PoolSource",
+                            fileNames = cms.untracked.vstring('/store/relval/CMSSW_12_2_0/RelValTTbar_14TeV/GEN-SIM-RECO/122X_mcRun3_2021_realistic_v5-v2/2580000/ac85ff65-dcea-4e7d-9995-d512f4bdafcf.root'
+                            )
+)
+
+process.trackPrinter = cms.EDAnalyzer("TrackDumper",
+    tracks = cms.untracked.InputTag("generalTracks")
+)
+
+process.p = cms.Path(process.trackPrinter)
+
+process.TFileService = cms.Service("TFileService", 
+                                   fileName = cms.string("histo.root"),
+                                   closeFileFast = cms.untracked.bool(True)
+                               )
